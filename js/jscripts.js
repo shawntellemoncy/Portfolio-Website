dc3127b2176fec0a20a4fd943316437f535b3eb8
Moncy <smoncy@it-rpub-mac5.eastern.ewu.edu>
2015-03-20 13:12:37 -0700

    Changes

---
+++ js/jscripts.js
@@ -0,0 +1,24 @@
+$('#photos').camera() {
+	autoPlay: true,
+	cameraPlay: true,
+	playPause: true,
+	pauseOnClick: true,
+	slideOn: 'random',
+	thumbnails: false,
+	time: 7000,
+	transPeriod: 1500,
+	autoAdvance: true,
+	hover: true
+	}
+
+
+
+
+
+
+
+
+
+
+<link href="index3.html">
+<script src="js/camera.js"></script>
