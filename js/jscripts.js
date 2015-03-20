6fb10e221fc8484923c8d87ad14daeb9d987541f
shawntellemoncy <smoncy@it-rpub-mac5.eastern.ewu.edu>
2015-03-20 13:23:37 -0700

    This sucks

--- js/jscripts.js
+++ js/jscripts.js
@@ -1,15 +1,3 @@
-$('#photos').camera() {
-	autoPlay: true,
-	cameraPlay: true,
-	playPause: true,
-	pauseOnClick: true,
-	slideOn: 'random',
-	thumbnails: false,
-	time: 7000,
-	transPeriod: 1500,
-	autoAdvance: true,
-	hover: true
-	}
 
 
 
@@ -20,5 +8,30 @@ $('#photos').camera() {
 
 
 
-<link href="index3.html">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <script src="js/camera.js"></script>
